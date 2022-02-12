19419dd1aa8deeb7fce13cba95b8467b16cc406a
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-02-12 13:18:01 +0900

    full screen cover의 헤더를 text field 로 구현

--- watchCook/Views/ContentView.swift
+++ watchCook/Views/ContentView.swift
@@ -11,26 +11,56 @@ func noop() {
     
 }
 
+class FormData: ObservableObject {
+    @Published var title: String = ""
+}
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var recipes: FetchedResults<Recipe>
+    @State private var selectedRecipe: Recipe?
+    @State private var isScreenCovered: Bool = false
+    
+    // MARK: form data
+    @StateObject private var formData: FormData = FormData()
+    
+    func selectRecipe(recipe: Recipe) {
+        selectedRecipe = recipe
+        formData.title = recipe.safeTitle
+        isScreenCovered = true
+    }
+    
+    func unselectRecipe() {
+        isScreenCovered = false
+        formData.title = ""
+        selectedRecipe = nil
+    }
     
     var body: some View {
         NavigationView {
             List(recipes) { recipe in
-                NavigationLink {
-                    RecipeDetail(recipe: recipe)
-                } label: {
-                    RecipeRow(text: recipe.title ?? "이름 없는 레시피")
+                Button(recipe.safeTitle) {
+                    selectRecipe(recipe: recipe)
                 }
             }
             .navigationTitle("레시피 목록")
             .toolbar {
-//                NavigationLink("레시피 추가", destination: RecipeDetail(recipe: Recipe()))
-                Button("레시피 추가") {
-                    _ = Recipe.randomInstance(context: moc)
-                    try? moc.save()
+                NavigationLink("레시피 추가", destination: RecipeDetail(recipe: Recipe()))
+            }
+            .fullScreenCover(isPresented: $isScreenCovered) {
+                // 이하를 RecipeDetail 로 집어넣고, FormData 상태도 RecipeDetail 에서 관리해야 함. onSubmit 시에 selectedRecipe의 속성값을 formData의 값으로 대치하는 식으로.
+                Spacer()
+                HStack {
+                    TextField("제목", text: $formData.title)
+//                    Text(item.safeTitle).font(.title).bold()
+                    Spacer()
+                    Button("완료") {
+                        unselectRecipe()
+                    }
                 }
+                .padding([.leading, .trailing], 16)
+                // steps only (title is not used)
+                RecipeDetail(recipe: selectedRecipe!)
             }
         }
     }
