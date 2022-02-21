0781da34086b086d3f9412bdef1e28fe7be976fb
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-02-21 20:51:47 +0900

    코드 정리: 워치 레시피 상세 페이지의 불필요한 게터 함수를 계산된 속성으로 변환

--- watchCook WatchKit Extension/RecipeDetail.swift
+++ watchCook WatchKit Extension/RecipeDetail.swift
@@ -11,23 +11,39 @@ struct RecipeDetail: View {
     @Environment(\.presentationMode) var presentation
     
     @State private var index: Int = -1
-    @State private var text: String = "준비되셨나요"
     @State private var currentStep: Step? = nil
     @State private var timerStartedAt: Date? = nil
     @State private var timerRemaining: Int32 = 0
 
     var recipe: Recipe
     
-    func getTimerRemaining() -> Int32 {
-        if let startedAt = timerStartedAt {
-            return max((currentStep?.seconds ?? 0) - Int32(Date().timeIntervalSince(startedAt)), 0)
-        }
-        return currentStep?.seconds ?? 0
-    }
+    // MARK: 계산되는 속성
     
-    func getTimerText() -> String {
+    var timerText: String {
         return TimeValue(seconds: timerRemaining).humanized
     }
+    
+    var text: String {
+        if (isStart()) {
+            return "준비되셨나요?"
+        } else if (isEnd()) {
+            return "수고하셨어요. 맛있게 드세요!"
+        } else {
+            return currentStep?.text ?? ""
+        }
+    }
+    
+    var primaryButtonText: String {
+        if isStart() {
+            return "시작하기"
+        } else if isEnd() {
+            return "끝내기"
+        } else {
+            return "다음"
+        }
+    }
+    
+    // MARK: 계산되는 속성으로 "아직" 변환하지 못한 게터 함수
     
     func isStart() -> Bool {
         return index == -1
@@ -37,6 +53,17 @@ struct RecipeDetail: View {
         return index == recipe.stepArray.count
     }
     
+    // MARK: 사용자의 액션 혹은 이벤트 처리
+    
+    func updateTimerRemaining() {
+        if let startedAt = timerStartedAt {
+            let elapsedSeconds = Int32(Date().timeIntervalSince(startedAt))
+            timerRemaining = max((currentStep?.seconds ?? 0) - elapsedSeconds, 0)
+        } else {
+            timerRemaining = currentStep?.seconds ?? 0
+        }
+    }
+    
     func nextStep() {
         if !isEnd() {
             index += 1
@@ -44,7 +71,7 @@ struct RecipeDetail: View {
                 currentStep = nil
             } else {
                 currentStep = recipe.stepArray[index]
-                timerRemaining = getTimerRemaining()
+                updateTimerRemaining()
             }
         }
     }
@@ -56,33 +83,23 @@ struct RecipeDetail: View {
                 currentStep = nil
             } else {
                 currentStep = recipe.stepArray[index]
-                timerRemaining = getTimerRemaining()
+                updateTimerRemaining()
             }
         }
     }
     
-    func getText() -> String {
-        if (isStart()) {
-            return "준비되셨나요?"
-        } else if (isEnd()) {
-            return "수고하셨어요. 맛있게 드세요!"
-        } else {
-            return currentStep?.text ?? ""
+    func startTimer() {
+        if timerStartedAt == nil {
+            timerStartedAt = Date()
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                if timerRemaining == 0 {
+                    timerStartedAt = nil
+                    timer.invalidate()
+                }
+                
+                updateTimerRemaining()
+            }
         }
-    }
-    
-    func getPrimaryButtonText() -> String {
-        if (isStart()) {
-            return "시작하기"
-        } else if (isEnd()) {
-            return "끝내기"
-        } else {
-            return "다음"
-        }
-    }
-    
-    func getSecondaryButtonText() -> String {
-        return "이전"
     }
     
     var body: some View {
@@ -91,35 +108,24 @@ struct RecipeDetail: View {
                 VStack {
                     Spacer()
                     
-                    Text(getText())
+                    Text(text)
                         .navigationTitle(recipe.safeTitle)
                     
                     Spacer()
                     
+                    // 남은 시간이 아니라 타이머 자체가 단계에 존재하는지 여부로 렌더해야 할 거 같다.
                     if timerRemaining > 0 {
-                        Button(getTimerText()) {
-                            if timerStartedAt == nil {
-                                timerStartedAt = Date()
-                                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                                    if timerRemaining == 0 {
-                                        timerStartedAt = nil
-                                        timer.invalidate()
-                                    }
-                                    
-                                    timerRemaining = getTimerRemaining()
-                                }
-                            }
-                        }
+                        Button(timerText, action: startTimer)
+                            .buttonStyle(.plain)
+                        Spacer()
                     }
 
                     HStack{
                         if !isStart() {
-                            Button(getSecondaryButtonText(), action: {
-                                prevStep()
-                            })
+                            Button("이전", action: prevStep)
                         }
                         
-                        Button(getPrimaryButtonText(), action: {
+                        Button(primaryButtonText, action: {
                             if !isEnd() {
                                 nextStep()
                             } else {
